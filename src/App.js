09d6bde65552021b39c494c8ee3927b7cdfd49e4
punit1808 <punityadav1808@gmail.com>
2025-07-17 14:36:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React,{useState,useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './App.css';
 import { Router, Routes, Route, Link, useNavigate  } from 'react-router-dom';
 import StartGame from './Component/StartGame';
